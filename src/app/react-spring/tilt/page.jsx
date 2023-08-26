7c90b61c8e366371cf547df09bfd3a6768ff1258
조성원 <tjddnjsjo@kakao.com>
2023-08-26 14:37:32 +0900

    feat: Add card tilt effect

---
+++ src/app/react-spring/tilt/page.jsx
@@ -0,0 +1,5 @@
+import TiltCard from "@/containers/tilt/TiltCard";
+
+export default function Tilt() {
+  return <TiltCard />;
+}
---
+++ src/containers/tilt/TiltCard.jsx
@@ -0,0 +1,46 @@
+"use client";
+
+import React, { useRef } from "react";
+import { useSpring, animated } from "@react-spring/web";
+
+const cardClassName = "w-64 h-96 bg-black rounded shadow-xl";
+
+const TiltCard = () => {
+  const divRef = useRef(null);
+
+  const [props, set] = useSpring(() => ({
+    xys: [0, 0, 1],
+    config: { mass: 5, tension: 2000, friction: 200 },
+  }));
+
+  const calc = (x, y) => [
+    -(y - divRef.current.offsetTop - divRef.current.clientHeight / 2) / 20,
+    (x - divRef.current.offsetLeft - divRef.current.clientWidth / 2) / 20,
+    1,
+  ];
+
+  const trans = (x, y, s) =>
+    `perspective(1000px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
+
+  const handleMouseMove = ({ clientX: x, clientY: y }) => {
+    set({ xys: calc(x, y) });
+  };
+
+  const handleMouseLeave = () => {
+    set({ xys: [0, 0, 1] });
+  };
+
+  return (
+    <animated.div
+      className={cardClassName}
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
+      ref={divRef}
+      style={{
+        transform: props.xys.to(trans),
+      }}
+    />
+  );
+};
+
+export default TiltCard;
