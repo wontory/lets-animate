737512e460788ef2cea6435acfb54d54aef0db8c
조성원 <tjddnjsjo@kakao.com>
2023-08-27 02:55:37 +0900

    refactor: Card children 속성 추가

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,14 @@
 import React, { forwardRef } from "react";
 
-const Card = forwardRef(({ className = "", ...props }, ref) => {
+const Card = forwardRef(({ className = "", children, ...props }, ref) => {
   return (
     <div
       className={`w-64 h-96 bg-black rounded shadow-xl ${className}`}
       ref={ref}
       {...props}
-    />
+    >
+      {children}
+    </div>
   );
 });
 
